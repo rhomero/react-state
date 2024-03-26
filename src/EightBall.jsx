1401c5b9f5f8d2d5fb9b5144d98388eb2deb6c2e
Homero Ramirez <homeroram@outlook.com>
2024-03-25 19:26:55 -0500

    add reset button

--- src/EightBall.jsx
+++ src/EightBall.jsx
@@ -13,13 +13,26 @@ function EightBall({ choice = defAnswers }) {
     setAnswer(prevAnswer => decision(choice));
   }
 
+  function resetClick(evt) {
+    setAnswer({
+      msg:"Think of a Question",
+      color:"black"
+    });
+  }
+
   return (
+    <>
     <div
       className="EightBall"
       onClick={handleClick}
       style={{backgroundColor: answer.color }}>
       <b>{answer.msg}</b>  
     </div>
+    <div
+      className="ResetContainer">
+    <button className="ResetButton"onClick={resetClick}>Reset</button>
+    </div>
+    </>
   );
 }
 
